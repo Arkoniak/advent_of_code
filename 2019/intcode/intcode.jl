b798407ecd30bfa3c41ca9ce4f11836ea37fe0f7
Andrey Oskin <skoffer@arkoniak.com>
2020-02-13 23:28:33 +0200

    faster intcode

--- 2019/intcode/intcode.jl
+++ 2019/intcode/intcode.jl
@@ -1,3 +1,11 @@
+module IntCode
+using StaticArrays
+struct FastHashInt{T<:Integer}; i::T; end
+
+Base.:(==)(x::FastHashInt, y::FastHashInt) = x.i == y.i
+Base.hash(x::FastHashInt, h::UInt) = xor(UInt(x.i), h)
+
+export Prog, run
 ############## IntCode ##############################
 # states are
 # 0 - running
@@ -12,109 +20,92 @@ mutable struct Prog
     relative_base::Int
     state::Int
     ascii::Bool
+    # modes::Dict{FastHashInt{Int}, Tuple{Int, Vector{Int}}}
+    modes::Vector{Tuple{Int, Tuple{Int, Int, Int}}}
 end
 
 str2prog(s) = parse.(Int, split(s, ","))
 
-Prog(s::String; mem = 10000, ascii = false) = Prog(vcat(str2prog(s), zeros(Int, mem)), 1, [], [], 0, 0, ascii)
+function Prog(s::String; mem = 10000, ascii = false)
+    modes = Vector{Tuple{Int, Tuple{Int, Int, Int}}}(undef, 22300)
+    for i in 0:2, j in 0:2, k in 0:2
+        for op in vcat(0:9, 99)
+            modes[10000*i + 1000*j + 100*k + op + 1] = (op, (k, j, i))
+        end
+    end
+    Prog(vcat(str2prog(s), zeros(Int, mem)), 1, [], [], 0, 0, ascii, modes)
+end
 
 copy_code(p::Prog) = Prog(copy(p.code), 1, [], [], 0, 0, p.ascii)
 
-struct Instruction
-    op::Int
-    modes::Vector{Int}
-end
-
-function Instruction(op_code::Int)
-    ops = Dict{Int, Int}(
-        1 => 3,
-        2 => 3,
-        3 => 1,
-        4 => 1,
-        5 => 2,
-        6 => 2,
-        7 => 3,
-        8 => 3,
-        9 => 1,
-        99 => 0
-    )
-    
-    op = mod(op_code, 100)
-    modes_code = div(op_code, 100)
-    modes = zeros(ops[op])
-    for i in 1:ops[op]
-        modes[i] = mod(modes_code, 10)
-        modes_code = div(modes_code, 10)
-    end
-    
-    Instruction(op, modes)
-end
-
-Instruction(prog::Prog) = Instruction(prog.code[prog.cur])
-
-function take(prog::Prog, mode, offset)
+@inline function take(prog::Prog, mode, offset)
     if mode == 0
-        return prog.code[prog.code[prog.cur + offset] + 1]
+        return @inbounds prog.code[prog.code[prog.cur + offset] + 1]
     elseif mode == 1
-        return prog.code[prog.cur + offset]
+        return @inbounds prog.code[prog.cur + offset]
     else
-        return prog.code[prog.code[prog.cur + offset] + prog.relative_base + 1]
+        return @inbounds prog.code[prog.code[prog.cur + offset] + prog.relative_base + 1]
     end
 end
 
-function update!(prog::Prog, value, offset, mode = 0)
+@inline function update!(prog::Prog, value, offset, mode = 0)
     if mode == 0
-        prog.code[prog.code[prog.cur + offset] + 1] = value
+        @inbounds prog.code[prog.code[prog.cur + offset] + 1] = value
     elseif mode == 2
-        prog.code[prog.code[prog.cur + offset] + prog.relative_base + 1] = value
+        @inbounds prog.code[prog.code[prog.cur + offset] + prog.relative_base + 1] = value
     end
 end
 
-function apply(prog::Prog, instruction::Instruction)
-    if instruction.op == 99
+function step(prog::Prog)
+    @inbounds op_code = prog.code[prog.cur]
+    op, modes = prog.modes[op_code + 1]
+
+    if op == 99
         prog.state = 99
-    elseif instruction.op == 1
-        a1 = take(prog, instruction.modes[1], 1)
-        a2 = take(prog, instruction.modes[2], 2)
-        update!(prog, a1 + a2, 3, instruction.modes[3])
+    elseif op == 1
+        a1 = take(prog, modes[1], 1)
+        a2 = take(prog, modes[2], 2)
+        update!(prog, a1 + a2, 3, modes[3])
         prog.cur += 4
-    elseif instruction.op == 2
-        a1 = take(prog, instruction.modes[1], 1)
-        a2 = take(prog, instruction.modes[2], 2)
-        update!(prog, a1 * a2, 3, instruction.modes[3])
+    elseif op == 2
+        a1 = take(prog, modes[1], 1)
+        a2 = take(prog, modes[2], 2)
+        update!(prog, a1 * a2, 3, modes[3])
         prog.cur += 4
-    elseif instruction.op == 3
+    elseif op == 3
         if isempty(prog.input)
             prog.state = 1
         else
-            update!(prog, popfirst!(prog.input), 1, instruction.modes[1])
+            update!(prog, popfirst!(prog.input), 1, modes[1])
             prog.cur += 2
         end
-    elseif instruction.op == 4
-        push!(prog.output, take(prog, instruction.modes[1], 1))
+    elseif op == 4
+        push!(prog.output, take(prog, modes[1], 1))
         prog.cur += 2
-    elseif instruction.op == 5
-        a1 = take(prog, instruction.modes[1], 1)
-        a2 = take(prog, instruction.modes[2], 2)
+    elseif op == 5
+        a1 = take(prog, modes[1], 1)
+        a2 = take(prog, modes[2], 2)
         prog.cur = a1 != 0 ? a2 + 1 : prog.cur + 3
-    elseif instruction.op == 6
-        a1 = take(prog, instruction.modes[1], 1)
-        a2 = take(prog, instruction.modes[2], 2)
+    elseif op == 6
+        a1 = take(prog, modes[1], 1)
+        a2 = take(prog, modes[2], 2)
         prog.cur = a1 == 0 ? a2 + 1 : prog.cur + 3
-    elseif instruction.op == 7
-        a1 = take(prog, instruction.modes[1], 1)
-        a2 = take(prog, instruction.modes[2], 2)
-        update!(prog, a1 < a2 ? 1 : 0, 3, instruction.modes[3])
+    elseif op == 7
+        a1 = take(prog, modes[1], 1)
+        a2 = take(prog, modes[2], 2)
+        update!(prog, a1 < a2 ? 1 : 0, 3, modes[3])
         prog.cur += 4
-    elseif instruction.op == 8
-        a1 = take(prog, instruction.modes[1], 1)
-        a2 = take(prog, instruction.modes[2], 2)
-        update!(prog, a1 == a2 ? 1 : 0, 3, instruction.modes[3])
+    elseif op == 8
+        a1 = take(prog, modes[1], 1)
+        a2 = take(prog, modes[2], 2)
+        update!(prog, a1 == a2 ? 1 : 0, 3, modes[3])
         prog.cur += 4
-    elseif instruction.op == 9
-        prog.relative_base += take(prog, instruction.modes[1], 1)
+    elseif op == 9
+        prog.relative_base += take(prog, modes[1], 1)
         prog.cur += 2
     end
+
+    prog.state
 end
 
 function run(prog::Prog, input::Vector{Int})
@@ -128,11 +119,8 @@ run(prog::Prog, input::String) = run(prog, Int.(collect(input)))
 
 function run(prog)
     clear!(prog)
-    while prog.state == 0
-        instruction = Instruction(prog)
-        apply(prog, instruction)
-    end
-    
+    while step(prog) == 0 end
+
     if prog.ascii
         return join(Char.(prog.output))
     else
@@ -140,9 +128,5 @@ function run(prog)
     end
 end
 
-function feed(prog::Prog, input::Vector{Int})
-    prog.input = vcat(prog.input, input)
-    prog.state = 0
-end
-
 clear!(prog) = (prog.output = [])
+end
